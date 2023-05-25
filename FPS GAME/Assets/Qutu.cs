cc7ff2f69d329b1727539152c04d31489f32d596
Lekkoulhs <124064265+Lekkoulhs@users.noreply.github.com>
2023-05-25 17:38:20 +0300

    Refinements

--- FPS GAME/Assets/Qutu.cs
+++ FPS GAME/Assets/Qutu.cs
@@ -1,11 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Qutu : MonoBehaviour
 {
-   public void quit()
+    public void quit()
     {
         Application.Quit();
+    }
+
+    [SerializeField] public string MenuScene;
+
+
+
+
+    public void GoMenu()
+    {
+
+        SceneManager.LoadScene(MenuScene);
     }
 }
