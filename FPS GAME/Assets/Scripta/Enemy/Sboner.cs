21cd073de416aa5f92ee412286911600721f852d
Lekkoulhs <124064265+Lekkoulhs@users.noreply.github.com>
2023-05-25 08:42:07 +0300

    diafora fixes and placemetns

--- FPS GAME/Assets/Scripta/Enemy/Sboner.cs
+++ FPS GAME/Assets/Scripta/Enemy/Sboner.cs
@@ -6,6 +6,10 @@ public class Sboner : MonoBehaviour
 {
 
     public GameObject enemy;
+    public GameObject secondaryenemy;
+    private int counter = 0;
+    public int howmanytype1 = 0;
+    public bool want2types = false;
     public float delay = 1f;
 
     // Start is called before the first frame update
@@ -15,7 +19,20 @@ public class Sboner : MonoBehaviour
     }
 
    public void SpawnEnemy()
-    {
-        Instantiate(enemy, transform.position, Quaternion.identity);
+    { 
+        if (want2types== false)
+        {
+            Instantiate(enemy, transform.position, Quaternion.identity);
+        }
+       else if (want2types== true && counter <= howmanytype1)
+        {
+            Instantiate(enemy, transform.position, Quaternion.identity);
+            counter++;
+        }
+        else
+        {
+            Instantiate(secondaryenemy, transform.position, Quaternion.identity);
+            counter = 0;
+        }
     }
 }
